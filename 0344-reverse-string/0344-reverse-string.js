303cd8d94f3a6a5051f8c413aaa31044352ace00
FAYIS T <154487822+fayis-t@users.noreply.github.com>
2025-04-05 09:19:49 +0530

    Time: 0 ms (100%), Space: 60.1 MB (61.85%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.js
@@ -0,0 +1,19 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let left = 0;;
+    let right = s.length-1;
+
+    while(left < right){
+        let prev = s[left];
+        s[left] = s[right];
+        s[right] = prev;
+
+        left++;
+        right--;
+    }
+    
+    return s;
+};
