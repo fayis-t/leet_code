8429ad8757ca5550adf2599f306dbbb5a6b39d00
FAYIS T <154487822+fayis-t@users.noreply.github.com>
2025-05-09 11:10:33 +0530

    Time: 1 ms (86.65%), Space: 53.2 MB (92.1%) - LeetHub

---
+++ 1217-relative-sort-array/1217-relative-sort-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @return {number[]}
+ */
+var relativeSortArray = function(arr1, arr2) {
+    let result = [];
+    for(let num of arr2){
+        while (arr1.includes(num)){
+            result.push(num);
+            arr1.splice(arr1.indexOf(num), 1);
+        }
+    }
+
+    arr1.sort((a, b) => a-b);
+    return result.concat(arr1);
+};
