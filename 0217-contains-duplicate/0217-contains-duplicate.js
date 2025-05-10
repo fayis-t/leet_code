7140a709443cb2fb9690774673942cf7a69ba56d
FAYIS T <154487822+fayis-t@users.noreply.github.com>
2025-05-10 17:36:23 +0530

    Time: 19 ms (35.7%), Space: 73.8 MB (25.86%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,11 +3,6 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-   const sorted = nums.sort((a,b) => a-b)
-    for(let i=0; i<nums.length; i++){
-        if(sorted[i] === sorted[i+1]){
-            return true;
-        }
-    }
-    return false;
+   const hashSet = new Set(nums);
+    return hashSet.size !== nums.length;
 };
