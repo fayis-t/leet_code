bfde20d6dc6e44d4e422c31ec1998d1604f466d5
FAYIS T <154487822+fayis-t@users.noreply.github.com>
2025-05-17 09:24:59 +0530

    Time: 5 ms (14.51%), Space: 53.8 MB (86.29%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    let result = [];
+    for(let i=0; i<nums1.length; i++){
+        for(let j=0; j<nums2.length; j++){
+            if(nums1[i] === nums2[j]){
+                if(!result.includes(nums1[i])){
+                    result.push(nums1[i]);
+                }
+            }
+        }
+    }
+    return result;
+};
