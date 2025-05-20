bf0fad2c9aedb9bf10b10ebcbd4a0ebe1c03071e
FAYIS T <154487822+fayis-t@users.noreply.github.com>
2025-05-20 11:22:56 +0530

    Time: 0 ms (100%), Space: 54.1 MB (32.13%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function(rowIndex) {
+    let result = [1];
+    for(let i=1; i<= rowIndex; i++){
+        result.push(result[i-1] * (rowIndex-i+1)/i);
+    }
+    return result;
+};
