4e3817dbdad3ca99c55877e3634c7d30f0481ca6
FAYIS T <154487822+fayis-t@users.noreply.github.com>
2025-05-23 20:42:37 +0530

    Time: 29 ms (73.12%), Space: 59.7 MB (62.06%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -3,8 +3,21 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {    
-    for(let i=0; i<s.length; i++){
-        if(s.indexOf(s[i]) === s.lastIndexOf(s[i])) return i;
+    // for(let i=0; i<s.length; i++){
+    //     if(s.indexOf(s[i]) === s.lastIndexOf(s[i])) return i;
+    // }
+    // return -1;
+
+    //  another method :
+    let map = {};
+
+    for(let char of s){
+        map[char] = (map[char] || 0) + 1;
     }
+
+    for(let i=0; i<s.length; i++){
+        if(map[s[i]] === 1) return i;
+    }
+    
     return -1;
 };
